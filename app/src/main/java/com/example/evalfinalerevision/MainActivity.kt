b3929ad92d2db123646999acfdf813e1dee275dd
Romek <129655936+romekdomasik@users.noreply.github.com>
2023-10-05 09:20:47 +0200

    bottom bar + custom color

--- app/src/main/java/com/example/evalfinalerevision/MainActivity.kt
+++ app/src/main/java/com/example/evalfinalerevision/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.evalfinalerevision
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.evalfinalerevision.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val bottomNavigationView = binding.bottomNavigationView
+        val navControler = findNavController(R.id.navHostFragment)
+        NavigationUI.setupWithNavController(bottomNavigationView, navControler)
     }
 }
